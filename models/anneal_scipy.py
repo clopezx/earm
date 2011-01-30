4b771b550d416e9b83bfe73a0cc78f64e29cb485
clopez <clopez@morpheus.(none)>
2011-01-30 15:43:40 -0500

    some more file rearrangement

---
+++ models/anneal_scipy.py
@@ -0,0 +1,11 @@
+import scipy.optimize
+import numpy
+
+
+
+# anneal(func, x0, args=(), schedule='fast', full_output=0, T0=None, Tf=9.9999999999999998e-13, 
+#        maxeval=None, maxaccept=None, maxiter=400, boltzmann=1.0, learn_rate=0.5, 
+#        feps=9.9999999999999995e-07, quench=1.0, m=1.0, n=1.0, lower=-100, upper=100, dwell=50)
+
+
+def expsimrmsd(simarray, exparray)
