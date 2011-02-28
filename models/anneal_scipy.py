1b1766dab25cd17403dc30081c47dcacaa6a02dd
clopez <clopez@morpheus.(none)>
2011-02-28 02:33:38 -0500

    added b-spline to code. figure out variance...

--- models/anneal_scipy.py
+++ models/anneal_scipy.py
@@ -1,4 +1,5 @@
 import scipy.optimize
+import scipy.interpolate
 import numpy
 import csv
 
@@ -39,18 +40,27 @@ def compare_data(array0, array1):
     if len(arr0shape) != len(arr1shape):
         raise SystemExit("comparing arrays of different dimensions")
     
-    # get ref array for regridding...
-    if arr0shape[0] > arr1shape[0]:
-        refarray = array0
-        otharray = array1
-    else:
-        refarray = array1
-        otharray = array0
+    # get the time range where the arrays overlap
+    rngmin = max(array0[:,0].min(), array1[:,0].min)
+    rngmax = min(array0[:,0].max(), array1[:,0].max)
+    rngmin = round(rngmin, -1)
+    rngmax = round(rngmax, -1)
+    
+    # use the smallest number of gridpoints between the arrays as
+    # the new gridset... this is prob good enough as long as the
+    # arrays are roughly similar in the number of points
+    # 
+    # FIXME: should this be an average of the gridsize?
+    ngrid = min(arr0shape[0], arr1shape[0])
+    ipgrid =  numpy.linspace(rngmin, rngmax, ngrid)
+    
+    #now create a b-spline of the data and fit it to desired range
+    tck = scipy.interpolate.splrep(array0[:,0], array0[:,1])
+    iparray0 = scipy.interpolate.splev(ipgrid, tck)
+    
+    tck = scipy.interpolate.splrep(array1[:,0], array1[:,1])
+    iparray1 = scipy.interpolate.splev(ipgrid, tck)
 
-    
-
-    
-    
     
     
     
