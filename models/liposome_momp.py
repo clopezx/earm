6a808272f96649ab62d55e21e549d26a0283ab71
clopez <clopez@morpheus.(none)>
2011-05-02 14:26:02 -0400

    added some changes to liposomes

--- models/liposome_momp.py
+++ models/liposome_momp.py
@@ -5,21 +5,44 @@ from pysb import *
 
 Model()
 
-# Define compartments for this model
+# Compartments section
 Compartment('soln', dimension = 3, size = XX, parent = None)
 Compartment('lipo_brane', dimension = 2, size = XX, parent = soln)
 Compartment('liposome', dimension = 3, size = XX, parent = lipo_brane)
 
-
-Monomer('tBid', ['b', 's'], {'s':['act', 'inact']})
-Monomer('Bax', ['b', 'm1', 'm2', 's'], {'s':['act', 'inact']})
+# Species monomer sections
+Monomer('tBid', ['b', 'state'], {'state':['act', 'inact']})
+Monomer('Bax', ['b', 'm1', 'm2', 'state'], {'state':['act', 'inact']})
 Monomer('Cc-m', ['b'])
 
+Parameter('[NAME]', [VALUE])
+
 # tBid diffuses to the liposome membrane
+Rule('tBid_to_brane',
+     tBid(b = None, s='act') ** soln <> tBid(b = None, s='act') ** lipo_brane,
+     ktbidbranef, ktbidbraner)
 
 # Bax binds to a tBid on the membrane, Bax becomes active
+Rule('Bax_to_Bidbrane',
+     Bax(b = None, m1 = None, m2 = None, state='inact') ** soln + 
+     tBid(b = None, s='act') ** libo_brane <>
+     Bax(b = 1, m1 = None, m2 = None, state='act') ** lipo_brane % 
+     tBid(b = 1, s='act') ** libo_brane,
+     kbaxbranef, baxbraner)
 
-# Active Bax brings another Bax onto the membrane
+# Active Bax brings another Bax onto the membrane, leaves it free
+# FIXME: Make this a complex then products reaction rather than a one-step thing?
+Rule('Bax_to_Baxbrane',
+     Bax(b = None, m1 = None, m2 = None, state='inact') ** soln +
+     Bax(b = 1, m1 = None, m2 = None, state='act') ** lipo_brane % 
+     tBid(b = 1, s='act') ** libo_brane <>
+     Bax(b = None, m1 = None, m2 = None, state='act') ** lipo_brane +
+     Bax(b = 1, m1 = None, m2 = None, state='act') ** lipo_brane % 
+     tBid(b = 1, s='act') ** libo_brane,
+     kbaxbranef, kbaxbraner)
+     
+     
+
 
 # Active Bax dimerize
 
