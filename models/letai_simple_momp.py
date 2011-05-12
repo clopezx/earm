e8453df2bab6d3fd0a88c25725a320a9e509fd7a
carlos <carlos@mouse.(none)>
2011-05-12 00:50:02 -0400

    set core letai simple model

--- models/letai_simple_momp.py
+++ models/letai_simple_momp.py
@@ -5,94 +5,35 @@ from pysb import *
 
 Model()
 
-# Compartments section. No compartments, based on earmv1.0
-#Compartment('lipo_brane', dimension = 2, size = XX, parent = soln)
-#Compartment('liposome', dimension = 3, size = XX, parent = lipo_brane)
-
 # Species monomer sections
-Monomer('tBid', ['b1','b2', 'state'], {'state':['A', 'inA']})
-Monomer('Bax', ['b', 'm1', 'm2', 'state'], {'state':['A', 'inA']})
-Monomer('Cc-m', ['b'])
+Monomer('tBid', ['b', 'state'], {'state':['A', 'I']})
+Monomer('Bax', ['b', 'm', 'state'], {'state':['A', 'I']})
+Monomer('CytC', ['b', 'state'], {'state':['mito', 'cyto']})
+Monomer('BaxPore')
 
 # Parameter section
 Parameter('[NAME]', [VALUE])
 
-# copy rules from TRAIL to tbid from earmv1.0
-Rule('tBid_to_brane',
-     tBid(b1 = None, b2=None, s='A') ** soln <> tBid(b = None, b2=None, s='A') ** lipo_brane,
-     ktbidbranef, ktbidbraner)
+# Bax binds to active tBid, Bax becomes Active
+Rule('Bax_to_Bid',
+     Bax(b = None, m1 = None,  state='I') + tBid(b = None, s='A')  <>
+     Bax(b = 1, m1 = None,  state='I') % tBid(b = 1,  s='A'),
+     kbaxbidf, baxbidr)
+Rule(Bax(b = 1, m1 = None,  state='I') % tBid(b = 1,  s='A') >>
+     Bax(b = None, m1 = None,  state='A') + tBid(b=None, s='A'),
+     kbaxbidc)
 
-# Bax binds to a tBid on the membrane, Bax becomes Aive
-Rule('Bax_to_Bidbrane',
-     Bax(b = None, m1 = None, m2 = None, state='I') ** soln + 
-     tBid(b1 = None, b2 = None, s='A') ** lipo_brane <>
-     Bax(b = 1, m1 = None, m2 = None, state='I') ** lipo_brane % 
-     tBid(b1 = 1, b2 = None s='A') ** lipo_brane,
-     kbaxbranef, baxbraner)
+# Active Bax aggregates into a pore
+Rule(Bax(b = None, m = None, state='A') + Bax(b = None, m = None, state='A') <>
+     Bax(b = None, m = 1, state='A') % Bax(b = None, m = 1, state='A'),
+     kbaxporef, kbaxporer)
+Rule(Bax(b = None, m = 1, state='A') % Bax(b = None, m = 1, state='A') >>
+     BaxPore(),
+     kbaxporec)
 
-# Active Bax brings another Bax onto the membrane, leaves it free
-# Two step process... should this be comparably fast to tBid binding?
-Rule('Bax_to_BaxbraneCplx',
-     Bax(b = None, m1 = None, m2 = None, state='I') ** soln +
-     Bax(b = 1, m1 = None, m2 = None, state='I') ** lipo_brane % 
-     tBid(b1 = 1, b2 = None s='A') ** lipo_brane <>
-     Bax(b = 2, m1 = None, m2 = None, state='I') ** lipo_brane %
-     Bax(b = 1, m1 = None, m2 = None, state='I') ** lipo_brane % 
-     tBid(b = 1, s='A') ** lipo_brane,
-     kbaxbranef, kbaxbraner)
-Rule('BaxbraneCplx_to_Baxbrane',
-     Bax(b = 2, m1 = None, m2 = None, state='I') ** lipo_brane %
-     Bax(b = 1  m1 = None, m2 = None, state='I') ** lipo_brane % 
-     tBid(b1 = 1, b2 = 2, s='A') ** lipo_brane >>
-     Bax(b = None, m1 = None, m2 = None, state='A') ** lipo_brane +
-     Bax(b = 1 m1 = None, m2 = None, state='I') ** lipo_brane % 
-     tBid(b1 = 1, b2 = 2, s='A') ** lipo_brane,
-     kbaxbranec)
-     
-# Active Bax dimerize
-Rule('Baxbrane_dimer',
-     Bax(b = None, m1 = None, m2 = None, state='A') ** lipo_brane +
-     Bax(b = None, m1 = None, m2 = None, state='A') ** lipo_brane <>
-     Bax(b = None, m1 = 1, m2 = None, state='A') ** lipo_brane %
-     Bax(b = None, m1 = None, m2 = 1, state='A') ** lipo_brane,
-     kdimf, kdimr)
-     
-# Active Bax trimerize
-Rule('Baxbrane_trimer',
-     Bax(b = None, m1 = 1, m2 = None, state='A') %
-     Bax(b = None, m1 = None, m2 = 1, state='A') +
-     Bax(b = None, m1 = None, m2 = None, state='A') ** lipo_brane <>
-     Bax(b = None, m1 = 1, m2 = None, state='A') %
-     Bax(b = None, m1 = 2, m2 = 1, state='A') %
-     Bax(b = None, m1 = None, m2 = 2, state='A') ** lipo_brane,
-     ktrimf, ktrimr)
 
-# Active Bax tetramerize
-# Active Bax pentamerize
-# Active Bax hexamerize
-# Active Bax heptamerize
 
-# CytC mimic released to the cytosol
-Rule('CytC_release_trimer1',
-     CytC(b = None) ** liposome +
-     Bax(b = None, m1 = 1, m2 = None, state='A') %
-     Bax(b = None, m1 = 2, m2 = 1, state='A') %
-     Bax(b = None, m1 = None, m2 = 2, state='A') ** lipo_brane <>
-     CytC(b = 3) ** liposome %
-     Bax(b = 3, m1 = 1, m2 = None, state='A') %
-     Bax(b = None, m1 = 2, m2 = 1, state='A') %
-     Bax(b = None, m1 = None, m2 = 2, state='A') ** lipo_brane,
-     kcyttrimf, kcyttrimr)
-Rule('CytC_release_trimer2',
-     CytC(b = 3) ** liposome %
-     Bax(b = 3, m1 = 1, m2 = None, state='A') %
-     Bax(b = None, m1 = 2, m2 = 1, state='A') %
-     Bax(b = None, m1 = None, m2 = 2, state='A') ** lipo_brane >>
-     CytC(b = None) ** soln +
-     Bax(b = None, m1 = 1, m2 = None, state='A') %
-     Bax(b = None, m1 = 2, m2 = 1, state='A') %
-     Bax(b = None, m1 = None, m2 = 2, state='A') ** lipo_brane,
-     kcyttrimc)
+
      
 
      
