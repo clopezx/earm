638f2d1ba98eaeb7337536fc9976d354816fee38
clopez <clopez@morpheus.(none)>
2011-06-19 23:44:07 -0400

    got the modules file to work, now to copy and make 2.0

---
+++ models/earm_1_0modules.py
@@ -0,0 +1,100 @@
+# EARM 1.0 MODULES
+# Notice the set_mons_parms global function is needed to recognize the monomer and 
+# parameter names in the present scope
+#
+# rec_to_bid: This module defines the interactions from the ligand insult (e.g. TRAIL)
+#             to Bid activation as per EARM 1.0
+#
+# fxrcsp_to_parp: This module defines what happens after the pore is activated and 
+#                 CytC and Smac are released
+#
+# Albeck JG, Burke JM, Spencer SL, Lauffenburger DA, Sorger PK, 2008
+# Modeling a Snap-Action, Variable-Delay Switch Controlling Extrinsic
+# Cell Death. PLoS Biol 6(12): e299. doi:10.1371/journal.pbio.0060299
+#
+# http://www.plosbiology.org/article/info:doi/10.1371/journal.pbio.0060299
+#
+#
+import pysb
+from pysbhelperfuncs import *
+
+def set_mons_parms(model, monomer_list):
+    # copy model parameters and value names to local scope
+    for i in range(len(monomer_list)):
+        globals()[monomer_list[i].name] = monomer_list[i]
+    for i in range(len(model.parameters)):
+        globals()[model.parameters[i].name] = model.parameters[i]
+
+def rec_to_bid(model, monomer_list):
+    # make Monomers/Parameters accessible
+    set_mons_parms(model, monomer_list)
+        
+    # RECEPTOR TO tBID
+    # =====================
+    # tBID Activation Rules
+    # ---------------------
+    #        L + R <--> L:R --> DISC
+    #        pC8 + DISC <--> DISC:pC8 --> C8 + DISC
+    #        Bid + C8 <--> Bid:C8 --> tBid + C8
+    # ---------------------
+    twostepconv(L(), R(), DISC(bf = None ), klrf, klrr, klrc)
+    twostepmod(DISC(), C8(state='pro'), C8(bf = None, state='A'), kdiscc8f, kdiscc8r, kdiscc8c)
+    twostepmod(C8(state='A'), Bid(state='U'), Bid(bf = None, state='T'), kc8bidf, kc8bidr, kc8bidc)
+    # ---------------------
+    # Inhibition Rules
+    # ---------------------
+    #        flip + DISC <-->  flip:DISC  
+    #        C8 + BAR <--> BAR:C8 CSPS
+    # ---------------------
+    simplebind(DISC(), flip(), kflipdiscf, kflipdiscr)
+    simplebind(BAR(), C8(state='A'), kbarc8f, kbarc8r)
+    # ---------------------
+
+def pore_to_parp(model, monomer_list):
+    # make Monomers/Parameters accessible 
+    set_mons_parms(model, monomer_list)
+
+    # FXR CASPASES CLEAVE PARP 
+    # ========================
+    # CytC, Smac release
+    # ----------------------
+    #        AMito + mCytoC <-->  AMito:mCytoC --> AMito + ACytoC  
+    #        AMito + mSmac <-->  AMito:mSmac --> AMito + ASmac  
+    #        ACytoC <-->  cCytoC
+    #        ASmac <-->  cSmac
+    # ----------------------
+    twostepmod(MitoP(state='A'), CytoC(state='mito'), CytoC(bf = None, state='A'), kmitopcytocMf, kmitopcytocMr, kmitopcytocMc)
+    twostepmod(MitoP(state='A'), Smac(state='mito'), Smac(bf = None, state='A'), kmitopsmacMf, kmitopsmacMr, kmitopsmacMc)
+    Rule('cytocCtoM', CytoC(bf = None, state = 'A') <> CytoC(bf=None, state = 'cyto'), kcytocMcytocCf, kcytocMcytocCr)
+    Rule('SmacMtoSmacC', Smac(bf = None, state='A') <> Smac(bf = None, state='cyto'), ksmacMsmacCf, ksmacMsmacCr)
+    # ---------------------------
+    # Apoptosome formation
+    # ---------------------------
+    #        Apaf + cCytoC <-->  Apaf:cCytoC --> aApaf + cCytoC
+    #        aApaf + pC9 <-->  Apop
+    #        Apop + pC3 <-->  Apop:pC3 --> Apop + C3
+    # ---------------------------
+    twostepmod(CytoC(state='cyto'), Apaf(state='I'), Apaf(bf = None, state = 'A'), kcytocCapaff, kcytocCapafr, kcytocCapafc)
+    onestepconv(Apaf(state='A'), C9(), Apop(bf=None), kapafc9f, kapafc9r)
+    twostepmod(Apop(), C3(state='pro'), C3(bf = None, state='A'), kapopc3f, kapopc3r, kapopc3c)
+    # -----------------------------
+    # Apoptosome related inhibitors
+    # -----------------------------
+    #        Apop + XIAP <-->  Apop:XIAP  
+    #        cSmac + XIAP <-->  cSmac:XIAP  
+    simplebind(Apop(), XIAP(), kapopxiapf, kapopxiapr)
+    simplebind(Smac(state='cyto'), XIAP(), ksmacxiapf, ksmacxiapr)
+    # ---------------------------
+    # Caspase reactions (effectors, inhibitors, and loopback initiators)
+    # ---------------------------
+    #        pC3 + C8 <--> pC3:C8 --> C3 + C8 CSPS
+    #        pC6 + C3 <--> pC6:C3 --> C6 + C3 CSPS
+    #        XIAP + C3 <--> XIAP:C3 --> XIAP + C3_U CSPS
+    #        PARP + C3 <--> PARP:C3 --> CPARP + C3 CSPS
+    #        pC8 + C6 <--> pC8:C6 --> C8 + C6 CSPS
+    # ---------------------------
+    twostepmod(C8(state='A'), C3(state='pro'), C3(bf = None, state='A'), kc8c3f, kc8c3r, kc8c3c)
+    twostepmod(C3(state='A'), C6(state='pro'), C6(bf = None, state='A'), kc3c6f, kc3c6r, kc3c6c)
+    twostepmod(XIAP(), C3(state = 'A'), C3(bf = None, state = 'ub'), kxiapc3f, kxiapc3r, kxiapc3c)
+    twostepmod(C3(state = 'A'), PARP(state='U'), PARP(bf = None, state='C'), kc3parpf, kc3parpr, kc3parpc)
+    twostepmod(C6(state='A'), C8(state='pro'), C8(bf = None, state = 'A'), kc6c8f, kc6c8r, kc6c8c)
