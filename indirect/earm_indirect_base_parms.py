9a3eaa31d37862540a627ac04ef1fd856bffcb14
Carlos Lopez <carlos_lopez@hms.harvard.edu>
2011-09-12 12:03:55 -0400

    rearrange to match model provenance figure

---
+++ indirect/earm_indirect_base_parms.py
@@ -0,0 +1,163 @@
+from pysb import *
+# Parameter section
+#
+# 8/3/2011 
+# The parameters for ['kbidCbidMf', 'kbidCbidMr', 'kbidbcl2f', 'kbidbcl2r', 'kc8bidf', 'kc8bidr', 'kc8bidc']
+# were optimized using simulated annealing to match the output of tBid in earm_1_5 to the 
+# original output of earm_1_0. I used a sigma of 5% (i.e. .05) for the objective function.
+#
+
+# Special parameters
+v = .07; # mitochondria compartment volume/cell volume
+
+# EARM 1.0 parameters
+# Reaction rates
+parameter_dict = {
+    #--------------------
+    # EARM 1.5 bcl2 module parameters
+    #--------------------
+    #Bax pore assembly
+    'BAX_PORE':   [[Parameter('kbaxdimf', 1.9e-05),
+                    Parameter('kbaxdimr', 0.00253168857302)],
+                   [Parameter('kbaxtrimf', 0.00012),
+                    Parameter('kbaxtrimr', 0.000455745565138)],
+                   [Parameter('kbaxtetf', 0.000116),
+                    Parameter('kbaxtetr', 0.000902098483308)]],
+    'BAK_PORE':   [[Parameter('kbakdimf', 1.98972134863e-05),
+                    Parameter('kbakdimr', 0.00117910083105)],
+                   [Parameter('kbaktrimf', 0.000120932227934 ),
+                    Parameter('kbaktrimr', 4.81022463147e-06)],
+                   [Parameter('kbaktetf',  0.000116488694321),
+                    Parameter('kbaktetr', 0.000743264410286)]],
+
+    # Inhibitions of Bax/Bak by Bcl2/BclxL/Mcl1
+    # These are used in the simple_bind_table function which expects
+    # row-major order (if you don't know what this means google it)
+    'BID_BAX_BAK_inh':[[Parameter('baxbclxlf', 2.53724317374e-06),#
+                        Parameter('baxbclxlr', 0.000777609558706  )],
+                       [Parameter('bakbclxlf', 0.000139640194286),#
+                        Parameter('bakbclxlr',0.000334860167879 )],
+                       [Parameter('bakmcl1f', 0.00011361077281),#
+                        Parameter('bakmcl1r', 0.0117982649382 )]],
+    # Sensitizers of Bcl2/BclxL/Mcl1 by Bad/NOXA
+    'BCLs_sens':      [[Parameter('kbadbclxlf', 5.02786873341e-05),
+                        Parameter('kbadbclxlr', 0.00029689476754 )],
+                       [Parameter('knoxabcl2f', 1.46064619002e-05),
+                        Parameter('knoxabcl2r', 0.00137190327654 )],
+                       [Parameter('knoxamcl1f', 5.97803309472e-06),
+                        Parameter('knoxamcl1r', 6.68821754476e-05 )]],
+    
+    #---------------------------
+    # EARM 1.0 legacy parameters
+    #---------------------------
+    # rec_to_bid module parameters
+    'L_R_DISC':   [ Parameter('klrf', 4e-07),       
+                    Parameter('klrr', 1e-03),
+                    Parameter('klrc', 1e-05)],
+    # C8 activation via DISC,
+    'DISC_C8':    [ Parameter('kdiscc8f', 1e-06),   
+                    Parameter('kdiscc8r', 1e-03),
+                    Parameter('kdiscc8c', 1e+00)],
+    # Bid cleavage by C8
+    'C8_BID':     [ Parameter('kc8bidf', 9.98683170e-08),
+                    Parameter('kc8bidr', 1.72707515e-03),
+                    Parameter('kc8bidc', 1.40842965e+00)],
+    # DISC inhibition by FLIP
+    'DISC_FLIP':  [ Parameter('kflipdiscf', 1e-06), 
+                    Parameter('kflipdiscr', 1e-03)],
+    # C8 inhibition by BAR
+    'BAR_C8':     [ Parameter('kbarc8f', 1e-06), 
+                    Parameter('kbarc8r', 1e-03)],
+
+    #---------------------------
+    # pore_to_parp module parameters
+    #---------------------------
+    # CytoC transport by Bax
+    'BAX_CYTC':   [[ Parameter('kbaxcytocMCf',  1.07052327914e-05),   #same as Bax-SMAC
+                     Parameter('kbaxcytocMCr',  0.00114074765938 ),   #same as Bax-SMAC
+                     Parameter('kbaxcytocMCc', 11.1174330138     )]], #same as Bax-SMAC
+    # Smac transport by Bax
+    'BAX_SMAC':   [[ Parameter('kbaxsmacCAf',   1.07052327914e-05),  #change to MC for consistency (mito to cyto)
+                     Parameter('kbaxsmacCAr',   0.00114074765938 ),
+                     Parameter('kbaxsmacCAc',  11.1174330138     )]],
+    # CytoC transport activation by Bak **
+    'BAK_CYTC':   [[ Parameter('kbakcytocMCf',  9.21063577757e-06 ),
+                     Parameter('kbakcytocMCr',  0.00234088295981  ),
+                     Parameter('kbakcytocMCc', 27.3582352876      )]],
+    # Smac transport by Bak  **
+    'BAK_SMAC':   [[ Parameter('kbaksmacCAf',  9.21063577757e-06),   #same as Bak-SMAC
+                     Parameter('kbaksmacCAr',  0.00234088295981 ),   #same as Bak-SMAC
+                     Parameter('kbaksmacCAc', 27.3582352876     )]], #same as Bak-SMAC
+    # CytoC activation
+    'CYTOC_ACT':  [ Parameter('kcytocCcytoAf', 0.0273684974471),  #same as SMAC_ACT
+                    Parameter('kcytocCcytoAr', 0.0136700510202)], #same as SMAC_ACT
+    # Smac activation
+    'SMAC_ACT':  [ Parameter('ksmacCsmacAf', 0.0273684974471),
+                   Parameter('ksmacCsmacAr', 0.0159988303507)],
+    # Apaf activation by CytC
+    'APAF_CYTC':  [ Parameter('kcytocCapaff', 5e-07),
+                    Parameter('kcytocCapafr', 1e-03),
+                    Parameter('kcytocCapafc', 1e+00)],
+    # Apop formation by Apaf + C9
+    'APOP_C9:APAF':[ Parameter('kapafc9f', 5e-08),
+                     Parameter('kapafc9r', 1e-03)],
+     # C3 activation by Apop
+    'APOP_C3':    [ Parameter('kapopc3f', 5e-09),
+                    Parameter('kapopc3r', 1e-03),
+                    Parameter('kapopc3c', 1e+00)],
+    # Apop inhibition by XIAP
+    'APOP_XIAP':  [ Parameter('kapopxiapf', 2e-06),
+                    Parameter('kapopxiapr', 1e-03)],
+    # XIAP inhibition by Smac
+    'SMAC_XIAP':  [ Parameter('ksmacxiapf', 3.0000000001e-06),
+                    Parameter('ksmacxiapr', 0.00184889755483)],
+    # C3 activation by C8
+    'C3_C8':      [ Parameter('kc8c3f', 1e-07), 
+                    Parameter('kc8c3r', 1e-03),
+                    Parameter('kc8c3c', 1e+00)],
+    # C6 activation by C3
+    'C6_C3':      [ Parameter('kc3c6f', 1e-06), 
+                    Parameter('kc3c6r', 1e-03),
+                    Parameter('kc3c6c', 1e+00)],
+    # C3 ubiquination tag by XIAP
+    'C3_XIAP':    [ Parameter('kxiapc3f', 2e-06),
+                    Parameter('kxiapc3r', 1e-03),
+                    Parameter('kxiapc3c', 1e-01)],
+    # PARP cleavage by C3
+    'PARP_C3':    [ Parameter('kc3parpf', 1e-06),
+                    Parameter('kc3parpr', 1e-02),
+                    Parameter('kc3parpc', 1e+00)],
+    # C8 activation by C6
+    'C8_C6':      [ Parameter('kc6c8f', 3e-08), 
+                    Parameter('kc6c8r', 1e-03),
+                    Parameter('kc6c8c', 1e+00)],
+
+    #---------------------------------
+    # EARM 1.0 HeLa initial conditions
+    # Non-zero initial conditions (in molecules per cell):
+    #---------------------------------
+    'INIT_AMTS':    [ Parameter('L_0'     ,  3000), # Ligand corresponding to 50 ng/ml SuperKiller TRAIL
+                      Parameter('R_0'     ,   200),   # TRAIL receptor 
+                      Parameter('flip_0'  , 1.0e2),  # Flip
+                      Parameter('C8_0'    , 2.0e4),   # procaspase-8 
+                      Parameter('BAR_0'   , 1.0e3),  # Bifunctional apoptosis regulator
+                      Parameter('Bid_0'   , 4.0e4),  # Bid
+                      Parameter('Bax_0'   , 0.8e5), # Bax
+                      Parameter('Bak_0'   , 0.2e5), # Bak
+                      Parameter('BclxL_0' , 2.0e4),  # cytosolic BclxL
+                      Parameter('Mcl1_0'  , 2.0e4),   # mitochondrial Mcl1  
+                      Parameter('Bad_0'   , 1.0e3),  # Bad
+                      Parameter('NOXA_0'  , 1.0e3),  # NOXA
+                      Parameter('CytoC_0' , 5.0e5),  # cytochrome c
+                      Parameter('Smac_0'  , 1.0e5),  # Smac    
+                      Parameter('Apaf_0'  , 1.0e5),  # Apaf-1
+                      Parameter('C3_0'    , 1.0e4),  # procaspase-3 (pro-C3)
+                      Parameter('C6_0'    , 1.0e4),  # procaspase-6 (pro-C6)  
+                      Parameter('C9_0'    , 1.0e5),  # procaspase-9 (pro-C9)
+                      Parameter('XIAP_0'  , 1.0e5),  # X-linked inhibitor of apoptosis protein  
+                      Parameter('PARP_0'  , 1.0e6),  # C3* substrate
+                      ]
+    }
+
+
+
---
+++ indirect/earm_indirect_bxbk.py
@@ -0,0 +1,109 @@
+from pysb import *
+from pysbhelperfuncs import *
+
+
+Model()
+# Monomers for all modules (including imported modules)
+# =====================================================
+Monomer('L', ['bf']) # Ligand
+Monomer('R', ['bf']) # Receptor
+Monomer('DISC', ['bf']) # DISC
+Monomer('flip', ['bf']) # flip
+Monomer('C8', ['bf', 'state'], {'state':['pro', 'A']}) # Csp 8, states: pro, active
+Monomer('BAR', ['bf']) # BAR
+Monomer('Bid', ['bf', 'state'], {'state':['U', 'T', 'M']}) # Bid, states: Untruncated, Truncated, truncated+Membrane
+Monomer('Bax', ['bf', 'bh3', 'd2', 'state'], {'state':['A']}) # Bax, states: Cytoplasm, Mitochondria, Active
+Monomer('Bak', ['bf', 'bh3', 'd2', 'state'], {'state':['A']}) # Bax, states: inactive+Membrane, Active
+Monomer('BclxL', ['bf', 'state'], {'state':['C', 'M']}) # BclxL states: cytoplasm, mitochondris
+Monomer('Mcl1', ['bf']) 
+Monomer('Bad', ['bf']) 
+Monomer('NOXA', ['bf']) 
+Monomer('CytoC', ['bf', 'state'], {'state':['M', 'C', 'A']})
+Monomer('Smac', ['bf', 'state'], {'state':['M', 'C', 'A']})
+Monomer('Apaf', ['bf', 'state'], {'state':['I', 'A']})
+Monomer('Apop', ['bf'])
+Monomer('C3', ['bf', 'state'], {'state':['pro', 'A', 'ub']}) # Csp 3, states: pro, active, ubiquitinated
+Monomer('C6', ['bf', 'state'], {'state':['pro', 'A']}) # Csp 6, states: pro, active
+Monomer('C9', ['bf'])
+Monomer('PARP', ['bf', 'state'], {'state':['U', 'C']}) # PARP, states: uncleaved, cleaved
+Monomer('XIAP', ['bf'])
+
+# EARM 1.0 Parameters and Modules 
+# ===============================
+from earm_1_5annbidsmacprms import parameter_dict as kd 
+import earm_1_0modules # Must be called after the Monomers and Parameters are defined
+
+# tBID to MOMP 
+# ======================
+# Bax/Bak are constitutively active
+# pore_assembly(Subunit, size, rates):
+pore_assembly(Bax(bf=None, state='A'), 4, kd['BAX_PORE'])
+pore_assembly(Bak(bf=None, state='A'), 4, kd['BAK_PORE'])
+
+# ------------------------------------
+# MOMP Inhibition
+# ------------------------------------
+# Bcl2 inhibitors of Bax, Bak, and Bid
+# a set of simple bind reactions:
+#        Inh + Act <--> Inh:Act
+# ------------------------------------
+simple_bind_table([[                                           BclxL,  Mcl1],
+                   [                                              {},    {}],
+                   [Bax, {'bh3':None, 'd2':None, 'state':'A'},   True, False],
+                   [Bak, {'bh3':None, 'd2':None, 'state':'A'},   True,  True]],
+                  kd['BID_BAX_BAK_inh'], model)
+
+# Sensitizers
+# Bcl2 sensitizers bind through a simple bind resction: 
+#        Inh + Act <--> Inh:Act
+# This goes through the list in row-major order (as it should be)
+# ---------------------------------------------------------------
+simple_bind_table([[          BclxL,  Mcl1],
+                   [             {},    {}],
+                   [Bad,  {},  True, False],
+                   [NOXA, {},  True,  True]],
+                  kd['BCLs_sens'], model)
+
+# Import necessary modules
+# ========================
+# Generate the Receptor to Bid section from the EARM 1.0 module
+earm_1_0modules.rec_to_bid(model, kd)
+# Generate the Pore to MOMP section from the EARM 1.0 module
+earm_1_0modules.pore_to_parp(model, kd)
+
+# Initial non-zero species
+# ========================
+Initial(L(bf=None), L_0)
+Initial(R(bf=None), R_0)
+Initial(flip(bf=None), flip_0)
+Initial(C8(bf=None, state='pro'), C8_0)
+Initial(BAR(bf=None), BAR_0)
+Initial(Bid(bf=None, state='U'), Bid_0)
+Initial(Bax(bf=None, bh3=None, d2=None, state='C'), Bax_0)
+Initial(Bak(bf=None, bh3=None, d2=None, state='M'), Bax_0)
+Initial(BclxL (bf=None, state='C'), BclxL_0)
+Initial(Mcl1(bf=None), Mcl1_0)
+Initial(Bad(bf=None), Bad_0)
+Initial(NOXA(bf=None), NOXA_0)
+Initial(CytoC(bf=None, state='M'), CytoC_0)
+Initial(Smac(bf=None, state='M'), Smac_0)
+Initial(Apaf(bf=None, state='I'), Apaf_0)
+Initial(C3(bf=None, state='pro'), C3_0)
+Initial(C6(bf=None, state='pro'), C6_0)
+Initial(C9(bf=None), C9_0)
+Initial(PARP(bf=None, state='U'), PARP_0)
+Initial(XIAP(bf=None), XIAP_0)
+
+# Observables
+# ===========
+# Fig 4B from Albeck observes these, normalizes and inverts them
+# Observe('Bid',   Bid(bf=None, state='U'))
+# Observe('PARP',  PARP(bf=None, state='U'))
+# Observe('Smac',  Smac(bf=None, state='mito'))
+# # This is what *should* be observed???
+Observe('tBid',  Bid(state='M'))
+Observe('cPARP', PARP(state='C'))
+Observe('cSmac', Smac(state='A'))
+Observe('cSmac_n', Smac(bf=None, state='A'))
+Observe('cSmac_X', Smac(bf=1, state='A') % XIAP(bf=1))
+
