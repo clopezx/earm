2f96f6fac2357e0d0b44055ceac186fd3a4b24ad
John Bachman <bachmanjohn@gmail.com>
2012-06-28 17:28:27 -0400

    MOMP-only modules for EARM2

---
+++ earm2/mito/earm2_direct.py
@@ -0,0 +1,18 @@
+"""'embedded together' w earm1.0"""
+
+from pysb import *
+from earm2 import macros
+from earm2 import earm2_modules
+from earm2 import albeck_modules
+
+Model()
+
+macros.momp_monomers()
+Observable('aBax', Bax(state='A'))
+Observable('cSmac', Smac(state='A'))
+
+# The specific MOMP model to use
+earm2_modules.direct()
+macros.momp_initial_conditions('direct', bid_state='T')
+
+
