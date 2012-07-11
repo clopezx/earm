a51daf3422db8c68626c7143f5b316d146c4fb13
John Bachman <bachmanjohn@gmail.com>
2012-07-11 13:45:17 -0400

    Added more observables

--- earm2/mito/howells2011.py
+++ earm2/mito/howells2011.py
@@ -14,14 +14,14 @@ shen_modules.howells2011()
 
 # Observables
 Observable('AcBax_', Bax(bf=None, state='A'))
-#Observable('Bax4_', MatchOnce(Bax(bf=None, s1=1, s2=4) %
-#                              Bax(bf=None, s1=2, s2=1) %
-#                              Bax(bf=None, s1=3, s2=2) %
-#                              Bax(bf=None, s1=4, s2=3)))
+Observable('Bax4_', MatchOnce(Bax(bf=None, s1=1, s2=4) %
+                              Bax(bf=None, s1=2, s2=1) %
+                              Bax(bf=None, s1=3, s2=2) %
+                              Bax(bf=None, s1=4, s2=3)))
 Observable('Bid_', Bid(bf=None))
-Observable('Bad_', Bad(bf=None))
+#Observable('Bad_', Bad(bf=None))
 Observable('Bcl2_', Bcl2(bf=None))
-Observable('Bcl2_Bad_', Bcl2(bf=1) % Bad(bf=1))
-Observable('Bcl2_Bid_', Bcl2(bf=1) % Bid(bf=1))
-Observable('Bcl2_Bax_', Bcl2(bf=1) % Bax(bf=1))
-
+Observable('Bad_Bcl2_', Bcl2(bf=1) % Bad(bf=1))
+Observable('Bid_Bcl2_', Bcl2(bf=1) % Bid(bf=1))
+Observable('Bax_Bcl2_', Bcl2(bf=1) % Bax(bf=1))
+Observable('pBad1433_', Bad(bf=None, state='C', serine='B'))
