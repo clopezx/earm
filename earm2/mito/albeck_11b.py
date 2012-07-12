3fa726942c53c84ee1c5a090ecfff52352945d29
John Bachman <bachmanjohn@gmail.com>
2012-07-12 14:30:54 -0400

    Removed ODE and param lists from Albeck models

--- earm2/mito/albeck_11b.py
+++ earm2/mito/albeck_11b.py
@@ -14,37 +14,3 @@ albeck_modules.albeck_11b(do_pore_transport=True)
 Observable('AcBax_', Bax(bf=None, state='A'))
 Observable('cSmac', Smac(state='C'))
 
-p_name_map = {
-    'bind_BidT_BaxC_to_BidTBaxC_kf': 'k(2)',
-    'bind_BidT_BaxC_to_BidTBaxC_kr': 'k_(2)',
-    'catalyze_BidTBaxC_to_BidT_BaxA_kc': 'kc(2)',
-    'bind_BidT_Bcl2_kf': 'k(4)',
-    'bind_BidT_Bcl2_kr': 'k_(4)',
-    'bind_BaxA_Bcl2_kf': 'k(5)',
-    'bind_BaxA_Bcl2_kr': 'k_(5)',
-    'bind_BaxA_SmacM_to_BaxASmacM_kf': 'k(3)',
-    'bind_BaxA_SmacM_to_BaxASmacM_kr': 'k_(3)',
-    'catalyze_BaxASmacM_to_BaxA_SmacC_kc': 'kc(3)',
-    'bind_C8A_BidU_to_C8ABidU_kf': 'k(1)',
-    'bind_C8A_BidU_to_C8ABidU_kr': 'k_(1)',
-    'catalyze_C8ABidU_to_C8A_BidT_kc': 'kc(1)'}
-s_index_map = [2, 4, 8, 6, 1, 9, 3, 10, 12, 5, 13, 11, 7]
-
-# The list of ODEs from John Albeck's Matlab file
-m_ode_list = [
-    '-k(1)*x(1)*x(2) +k_(1)*x(9)+kc(1)*x(9)',
-    '-k(1)*x(1)*x(2) +k_(1)*x(9)',
-    'kc(1)*x(9) -k(2)*x(3)*x(4) +k_(2)*x(10)+kc(2)*x(10) - k(4)*x(3)*x(8)+ k_(5)*x(12)',
-    '-k(2)*x(3)*x(4)+k_(2)*x(10)',
-    'kc(2)*x(10) -  k(3)*x(5)*x(6)  + k_(3)*x(11) +kc(3)*x(11) - k(5)*x(5)*x(8)+ k_(5)*x(13)',
-    '-k(3)*x(5)*x(6) +k_(3)*x(11)',
-    'kc(3)*x(11)',
-    '-k(4)*x(3)*x(8)+k_(4)*x(12) - k(5)*x(5)*x(8)+k_(5)*x(13)',
-    'k(1)*x(1)*x(2)-k_(1)*x(9)-kc(1)*x(9)',
-    'k(2)*x(3)*x(4)-k_(2)*x(10)-kc(2)*x(10)',
-    'k(3)*x(5)*x(6)-k_(3)*x(11)-kc(3)*x(11)',
-    'k(4)*x(3)*x(8)-k_(4)*x(12)',
-    'k(5)*x(5)*x(8)-k_(5)*x(13)']
-m_ode_list = [parse_expr(ode) for ode in m_ode_list]
-
-
