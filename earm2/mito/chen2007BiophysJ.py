d86d6705c0caad3b9b9214702c65f70cce5a094d
John Bachman <bachmanjohn@gmail.com>
2012-07-11 22:41:52 -0400

    Hysteresis plot includes both up and down

--- earm2/mito/chen2007BiophysJ.py
+++ earm2/mito/chen2007BiophysJ.py
@@ -16,9 +16,11 @@ shen_modules.momp_monomers()
 #shen_modules.momp_initial_conditions(bid_state='T')
 
 # The specific MOMP model to use
-shen_modules.chen2007BiophysJ(pore_assembly=True)
+shen_modules.chen2007BiophysJ(pore_assembly=False)
 
 Observable('aBax_', Bax(state='A', bf=None))
+#Observable('aBax_', Bax(state='A', bf=None, s1=None, s2=None))
+#Observable('aBax_', Bax(state='A'))
 Observable('Bid_', Bid(bf=None))
 Observable('Bcl2_', Bcl2(bf=None))
 Observable('Bcl2_Bid_', Bcl2(bf=1) % Bid(bf=1))
@@ -39,19 +41,33 @@ def simulate(f2=0.5, tmax=3000):
     return x
 
 def bistability_analysis():
-    f2_range = linspace(0, 1, 20)
+    f2_range = linspace(0, 0.4, 41)
     t = linspace(0, 50000, 1000)
     ion()
-    figure()
-    ss_aBax_vals = []
+    ss_aBax_vals_up = []
+    ss_aBax_vals_down = []
 
     for f2 in f2_range:
         model.parameters['Bid_0'].value = f2 * 1e-1
+        bax_total = 2e-1
+
+        # Do "up" portion of hysteresis plot
+        model.parameters['aBax_0'].value = 0
+        model.parameters['cBax_0'].value = bax_total
         x = odesolve(model, t)
-        plot(t, x['aBax_']/model.parameters['Bax_0'].value)
-        ss_aBax_val = x['aBax_'][-1]/model.parameters['Bax_0'].value
-        ss_aBax_vals.append(ss_aBax_val)
+        figure('up')
+        plot(t, x['aBax_']/bax_total)
+        ss_aBax_vals_up.append(x['aBax_'][-1]/bax_total)
+
+        # Do "down" portion of hysteresis plot
+        model.parameters['aBax_0'].value = bax_total
+        model.parameters['cBax_0'].value = 0
+        x = odesolve(model, t)
+        figure('down')
+        plot(t, x['aBax_']/bax_total)
+        ss_aBax_vals_down.append(x['aBax_'][-1]/bax_total)
 
     figure()
-    plot(f2_range, ss_aBax_vals)
+    plot(f2_range, ss_aBax_vals_up, 'r')
+    plot(f2_range, ss_aBax_vals_down, 'g')
 
