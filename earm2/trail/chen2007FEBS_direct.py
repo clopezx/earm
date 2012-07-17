9809c022d5298dcd5faa45420d6e67d42da13731
John Bachman <bachmanjohn@gmail.com>
2012-07-16 23:51:14 -0400

    Chen2007 models all have full TRAIL implementations!

--- earm2/trail/chen2007FEBS_direct.py
+++ earm2/trail/chen2007FEBS_direct.py
@@ -1,11 +1,11 @@
-"""Model from Chen 2007, Biophys J."""
+"""Model from Chen 2007, FEBS Letters."""
 
 from pysb import *
 from earm2 import macros
 from earm2 import albeck_modules
 from earm2 import shen_modules
 import re
-from earm2.util import convert_um_to_num, convert_um_kf_to_stoch
+from earm2.util import convert_nm_to_num, convert_nm_kf_to_stoch
 
 Model()
 
@@ -15,17 +15,17 @@ shen_modules.momp_monomers()
 albeck_modules.apaf1_to_parp_monomers()
 
 # The specific MOMP model to use
-shen_modules.chen2007BiophysJ(do_pore_assembly=True, do_pore_transport=True)
+shen_modules.chen2007FEBS_direct(do_pore_assembly=True, do_pore_transport=True)
 
-# Set initial condition for uncleaved Bid to 0.1uM, per the paper
-Initial(Bid(state='U', bf=None), Parameter('Bid_0', 0.1))
+# Set initial condition for uncleaved Bid to 20nM, per the paper
+Initial(Bid(state='U', bf=None), Parameter('Bid_0', 20))
 
 # A hack--convert all parameters from um to # of molecules
 for p in model.parameters_initial_conditions():
-    p.value = convert_um_to_num(p.value)
+    p.value = convert_nm_to_num(p.value)
 for p in model.parameters:
     if (re.match('.*_kf$', p.name)):
-        p.value = convert_um_kf_to_stoch(p.value)
+        p.value = convert_nm_kf_to_stoch(p.value)
 
 # Now that we've converted the original parameters to numbers, we can load
 # the rest of the model
