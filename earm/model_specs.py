54ab5d868d79be33b4303775d8dac5f1b4a01c3e
Jeremy Muhlich <jmuhlich@bitflood.org>
2012-07-27 17:57:59 -0400

    make model_specs print out in table format

--- earm/model_specs.py
+++ earm/model_specs.py
@@ -6,11 +6,9 @@ models = ['lopez_direct', 'lopez_indirect', 'lopez_embedded',
           'cui_direct', 'cui_direct1', 'cui_direct2',
           'howells']
 
+print '%-24s %8s %8s' % ('Model', 'Rules', 'ODEs')
 for model in models:
     exec('import earm.mito.%s' % model)
     exec('m = earm.mito.%s.model' % model)
-    print("----------------------")
-    print("Model: %s" % model)
-    print("Number of rules: %d" % len(m.rules))
     generate_equations(m)
-    print("Number of odes: %d" % len(m.odes))
+    print "%-24s %8d %8d" % (model, len(m.rules), len(m.odes))
