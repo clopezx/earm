4244c0f94709f2243b08e56990a94bf3fd005989
John Bachman <bachmanjohn@gmail.com>
2012-08-08 11:39:36 -0400

    Code exmaples for macros figure

--- figures/fig2/catalyze_one_step_example.py
+++ figures/fig2/catalyze_one_step_example.py
@@ -1,5 +1,5 @@
 from pysb import *
-from pysb.macros import catalyze
+from pysb.macros import catalyze_one_step
 from pysb.bng import generate_network, generate_equations
 
 Model()
@@ -7,9 +7,10 @@ Model()
 Monomer('C8', ['bf'])
 Monomer('Bid', ['bf', 'state'], {'state': ['C', 'T']})
 
-klist = [Parameter('kf', 1), Parameter('kr', 1), Parameter('kc', 1)]
+kf = Parameter('kf', 1)
 
-catalyze(C8, 'bf', Bid(state='C'), 'bf', Bid(state='T'), klist)
+catalyze_one_step(C8(bf=None), Bid(state='C', bf=None),
+                  Bid(state='T', bf=None), kf)
 
 Initial(C8(bf=None), Parameter('C8_0', 100))
 Initial(Bid(bf=None, state='C'), Parameter('Bid_0', 100))
