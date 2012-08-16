cb5bb3738dd8acef311fda40442134a80ba3bd6a
John Bachman <bachmanjohn@gmail.com>
2012-08-16 11:10:10 -0400

    Compressed code for embedded_example

--- figures/fig5/embedded_example.py
+++ figures/fig5/embedded_example.py
@@ -14,38 +14,32 @@ def embedded():
 
     # tBid, Bax, and BclXL translocate to the membrane
     free_Bax = Bax(bf=None, s1=None, s2=None) # Alias
+
     equilibrate(Bid(bf=None, state='T'), Bid(bf=None, state='M'), [1e-1, 1e-3])
     equilibrate(free_Bax(state='C'), free_Bax(state='M'), transloc_rates)
     equilibrate(BclxL(bf=None, state='C'), BclxL(bf=None, state='M'), transloc_rates)
-
-    # tBid activates Bax and Bak
     catalyze(Bid(state='M'), Bax(state='M'), Bax(state='A'), bid_effector_rates)
     catalyze(Bid(state='M'), Bak(state='M'), Bak(state='A'), bid_effector_rates)
-
-    # Binding of tBid, Bad, and Noxa to Bcl2, Mcl1, and Bcl-XL
-    bind_table([[                       Bcl2,  BclxL(state='M'),  Mcl1(state='M')],
-                [Bid(state='M'),  bcl2_rates,        bcl2_rates,       bcl2_rates],
-                [Bad(state='M'),  bcl2_rates,        bcl2_rates,             None],
-                [NOXA(state='M'),       None,              None,       bcl2_rates]])
-
-    # Autoactivation: Bax and Bak activate their own kind, but only when
-    # free (i.e. not part of a pore complex)
+    bind_table([[                           Bcl2,  BclxL(state='M'),  Mcl1(state='M')],
+                [Bid(state='M'),      bcl2_rates,        bcl2_rates,       bcl2_rates],
+                [Bad(state='M'),      bcl2_rates,        bcl2_rates,             None],
+                [Noxa(state='M'),           None,              None,       bcl2_rates],
+                [Bax(active_monomer), bcl2_rates,        bcl2_rates,             None],
+                [Bak(active_monomer),       None,        bcl2_rates,       bcl2_rates]])
     catalyze(Bax(active_monomer), Bax(state='C'), Bax(state='A'), effector_auto_rates)
     catalyze(Bak(active_monomer), Bak(state='M'), Bak(state='A'), effector_auto_rates)
-
-    # tBid and free Bax recruit Bcl-xL
     catalyze(Bid(state='M'), BclxL(state='C'), BclxL(state='M'),
              bclxl_recruitment_rates)
     catalyze(Bax(active_monomer), BclxL(state='C'), BclxL(state='M'),
              bclxl_recruitment_rates)
-
-    # Anti-apoptotics bind activated effectors
-    # Doug Green's "MODE 2" inhibition
-    bind_table([[                            Bcl2,  BclxL(state='M'),        Mcl1],
-                [Bax(active_monomer),  bcl2_rates,        bcl2_rates,        None],
-                [Bak(active_monomer),        None,        bcl2_rates,  bcl2_rates]])
-
-    # Bax and Bak form pores by sequential addition
     assemble_pore_sequential(Bax(bf=None, state='A'), 4, pore_rates)
     assemble_pore_sequential(Bak(bf=None, state='A'), 4, pore_rates)
+
+    # 1. tBid activates Bax and Bak
+    # 2. Binding of tBid, Bad, Noxa, Bax and Bak to Bcl2, Mcl1, and Bcl-XL
+    # 3. Autoactivation: Bax and Bak activate their own kind, but only when
+    # 4. free (i.e. not part of a pore complex)
+    # 5. tBid and free Bax recruit Bcl-xL
+    # 6. Bax and Bak form pores by sequential addition
+
 
