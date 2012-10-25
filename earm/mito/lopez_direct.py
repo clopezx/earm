a5f936974b67822bf2d8c2bfeaa2feb364a22d1d
John Bachman <bachmanjohn@gmail.com>
2012-10-24 20:40:58 -0400

    Fixed observable for Bid by changing from mistaken "Bax"
    For some reason the mBid observable in each of these files was
    actually listed as Bax.

--- earm/mito/lopez_direct.py
+++ earm/mito/lopez_direct.py
@@ -10,7 +10,7 @@ from earm import albeck_modules
 Model()
 
 lopez_modules.momp_monomers()
-Observable('mBid', Bax(state='M'))
+Observable('mBid', Bid(state='M'))
 Observable('aBax', Bax(state='A'))
 Observable('cSmac', Smac(state='C'))
 
