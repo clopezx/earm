780460087e932b721ac048a29b212f33e95ac67c
Jeremy Muhlich <jmuhlich@bitflood.org>
2012-12-11 13:45:25 -0500

    small improvements to anneal_new.py

--- anneal_new.py
+++ anneal_new.py
@@ -1,6 +1,4 @@
-import earm_anneal
 import pysb.integrate
-import pysb.util
 import numpy as np
 import scipy.optimize
 import matplotlib.pyplot as plt
@@ -9,8 +7,8 @@ import os
 from earm.lopez_embedded import model
 
 
-obs_names = ('mBid', 'aSmac', 'cPARP')
-data_names = ('norm_ICRP', 'IMSRP_step', 'norm_ECRP')
+obs_names = ['mBid', 'aSmac', 'cPARP']
+data_names = ['norm_ICRP', 'IMSRP_step', 'norm_ECRP']
 
 def objective_func(x, solver, rate_mask, data):
     param_values = np.array([p.value for p in model.parameters])
@@ -46,9 +44,9 @@ if __name__ == '__main__':
     earm_path = os.path.join(os.path.dirname(__file__))
     data_path = os.path.join(earm_path, 'xpdata', 'forfits',
                              'EC-RP_IMS-RP_IC-RP_data_for_models.csv')
-    data = np.genfromtxt(data_path, delimiter=',', names=True)
+    exp_data = np.genfromtxt(data_path, delimiter=',', names=True)
 
-    solver = pysb.integrate.Solver(model, data['Time'], rtol=1e-3, atol=1e-3)
+    solver = pysb.integrate.Solver(model, exp_data['Time'], rtol=1e-3, atol=1e-3)
 
     np.random.seed(1)
     (xmin, Jmin, Tfinal, feval, iters, accept, retval) = \
@@ -58,16 +56,13 @@ if __name__ == '__main__':
     params_estimated = np.array([p.value for p in params])
     params_estimated[rate_mask] = 10 ** xmin
 
-
-    exp_data = np.load('xpdata/forfits/EC-RP_IMS-RP_IC-RP_data_for_models.npz')['arr_0']
-    exp_time = exp_data[0]
-    exp_obs_norm = exp_data[(2,5,8), :]
+    exp_obs_norm = exp_data[data_names].view(float).reshape(len(exp_data), -1).T
 
     solver.run(params_estimated)
     sim_obs_norm = (solver.yobs_view / solver.yobs_view.max(0)).T
 
     colors = ('r', 'g', 'b')
     for exp, sim, c in zip(exp_obs_norm, sim_obs_norm, colors):
-        plt.plot(exp_time, exp, color=c, marker='.', linestyle=':')
-        plt.plot(exp_time, sim, color=c)
+        plt.plot(exp_data['Time'], exp, color=c, marker='.', linestyle=':')
+        plt.plot(exp_data['Time'], sim, color=c)
     plt.show()
